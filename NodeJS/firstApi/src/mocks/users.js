4c9a1a1a07f72b6c2c0e093b18f6d1ee8d0442b0
FernandoFernandes05 <fer.fernandes.moraes05@gmail.com>
2025-02-07 00:13:51 -0300

    feat: firstApi, controllers and routes

---
+++ NodeJS/firstApi/src/mocks/users.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        id: 1,
+        name: 'Fernando',
+    },
+    {
+        id: 2,
+        name: 'Marcella',
+    },
+    {
+        id: 3,
+        name: 'Guilherme',
+    },
+]
